bebe35cdbeea4c0941a160efd92dcb8a7f83c275
sm riad <riad@abatacommerce.com>
2022-01-12 17:48:45 +0800

    user update

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // normal funtion:
 //function rootCall(req, res){
@@ -13,6 +18,22 @@ const app = express();
 
 app.get('/', (req, res)=> {
     res.send('thank you very much');
+})
+
+const users = ["Asad", "Khaled", "John", "Nazat", "Adam"]
+
+app.get('/users/:id', (req, res)=>{
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id,name});
+})
+
+//post 
+
+app.post('/addUser', (req, res)=>{
+    console.log('data received', req.body);
+    //post to database
+    res.send(req.body);
 })
 
 app.listen(3000, ()=> console.log('Listening to port 3000'))
